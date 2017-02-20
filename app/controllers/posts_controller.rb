b2528d140738bff05ea49b9703cb4e410e4cbba8
Ji Xiang <jx4554@gmail.com>
2017-02-20 09:49:33 +0800

    user simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.update(post_params)
+
+    if @post.save
+      redirect_to account_posts_path, notice: "Post Updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post destroy"
+
+  end
+
   private
 
   def post_params
